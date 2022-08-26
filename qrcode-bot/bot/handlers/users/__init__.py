8c1b2e16d18b541328f168d622cbb7089841629a
dfshsu <108235877+dfshsu@users.noreply.github.com>
2022-08-27 02:26:28 +0800

    Delete qrcode-bot/bot directory

--- qrcode-bot/bot/handlers/users/__init__.py
+++
@@ -1,5 +0,0 @@
-from .upload_qrcode import dp
-from .start import dp
-from .create_qrcode import dp
-
-__all__ = ['dp']
--- qrcode-bot/bot/handlers/users/start.py
+++
@@ -1,8 +0,0 @@
-from aiogram.types import Message
-from aiogram import types
-
-from loader import dp
-
-@dp.message_handler(commands=["start"])
-async def get_photo(message: types.Message):
-    await message.answer('Напиши /upload или просто отправь мне Qr код!')
